96e40bf8d6401545f3caa437bf3711484eaf3b1c
Levi Ackerman <diiego201124@gmail.com>
2025-08-21 23:37:38 -0400

    actualizacion base de datos

--- tienda_aseo/settings.py
+++ tienda_aseo/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'tienda_aseo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tienda_aseo_db',
+        'USER': 'tienda_user',        
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',           
+        'PORT': '5432',
     }
 }
 
